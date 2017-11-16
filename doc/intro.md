c54cfefc9f7afd597394e50bc66cff2afee54aca
Nick Hathaway <Nicholashathaway@gmail.com>
2017-11-15 23:16:03 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to imdb-scraper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/imdb_scraper/core.clj
@@ -0,0 +1,9 @@
+(ns imdb-scraper.core)
+(use 'criterium.core)
+
+(bench (Thread/sleep 1000))
+
+(defn -main
+  "Hey"
+  []
+  (println "Hello, World!"))
