44aa4cd6d1195715565bd0066d76e20aeafab714
Nick Hathaway <Nicholashathaway@gmail.com>
2017-11-16 01:00:34 -0700

    scrape one page using falcon

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject imdb-scraper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "concurrently scrapes imdb movie reviews"
+  :url "#"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"], [criterium "0.4.4"]]
+  :dependencies [[org.clojure/clojure "1.8.0"], [clj-http "3.7.0"], [falcon "0.1.0"]]
   :main imdb-scraper.core)
--- src/imdb_scraper/core.clj
+++ src/imdb_scraper/core.clj
@@ -1,9 +1,29 @@
-(ns imdb-scraper.core)
-(use 'criterium.core)
+(ns imdb-scraper.core
+  (:require [clj-http.client :as client]
+            [falcon.core :as falcon]))
 
-(bench (Thread/sleep 1000))
+; testing data for concurrency
+; 100 reviews from 10 pages 
+(def reviewUrls `("http://www.imdb.com/title/tt0093870/reviews?start=0" "http://www.imdb.com/title/tt0093870/reviews?start=10" "http://www.imdb.com/title/tt0093870/reviews?start=20" "http://www.imdb.com/title/tt0093870/reviews?start=30" "http://www.imdb.com/title/tt0093870/reviews?start=40" "http://www.imdb.com/title/tt0093870/reviews?start=50" "http://www.imdb.com/title/tt0093870/reviews?start=60" "http://www.imdb.com/title/tt0093870/reviews?start=70" "http://www.imdb.com/title/tt0093870/reviews?start=80" "http://www.imdb.com/title/tt0093870/reviews?start=90"))
+
+; writes a lazy sequence to a filename
+(defn writeToFile [filename, lazySeq]
+  (spit filename (pr-str lazySeq)))
+
+; takes a falcon html map and returns all containing text
+(defn extractReviewTxt [html]
+  (for [review html]
+       (get review :text)))
+
+; fetches and extracts text of p elements for a url
+(defn getReviewHtml [url]
+  (extractReviewTxt
+    (falcon/select
+      (falcon/parse url)
+      "p")))
 
 (defn -main
-  "Hey"
   []
-  (println "Hello, World!"))
+  (writeToFile
+    "robocop_page_6.txt"
+    (getReviewHtml "http://www.imdb.com/title/tt0093870/reviews?start=50")))
